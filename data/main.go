f9345a8ffe8c10d883e5c55e641e620faa114e46
dipilihincom <dipilihincom@gmail.com>
2025-02-18 21:34:04 +0000

    feat(auth): Add auth to rest api

--- data/main.go
+++ data/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"data/models"
+	"data/routes"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
+
+func main() {
+	models.ConnectDatabase()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "7302"
+	}
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	routes.Route(app)
+
+	app.Listen(":" + port)
+}
