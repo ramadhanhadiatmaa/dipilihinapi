710e38d4139ca2999d23dddf8136be35fc71c247
dipilihincom <dipilihincom@gmail.com>
2025-03-02 10:33:04 +0000

    refactor(data): Add middleware

--- chat/routes/route.go
+++ chat/routes/route.go
@@ -2,16 +2,17 @@ package routes
 
 import (
 	"chat/controllers"
+	"chat/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func Route(app *fiber.App) {
-	api := app.Group("/v1"/* , middlewares.Auth */)
+	api := app.Group("/v1")
 
 	chat := api.Group("/chat")
-	chat.Post("/", controllers.Create)
-	chat.Get("/", controllers.Show)
-	chat.Get("/:id", controllers.Index)
-	chat.Delete("/:id", controllers.Delete)
+	chat.Post("/", controllers.Create, middlewares.Auth)
+	chat.Get("/", controllers.Show, middlewares.Auth)
+	chat.Get("/:id", controllers.Index, middlewares.Auth)
+	chat.Delete("/:id", controllers.Delete, middlewares.Auth)
 }
--- data/routes/route.go
+++ data/routes/route.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"data/controllers"
+	"data/middlewares"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -10,15 +11,15 @@ func Route(app *fiber.App) {
 	api := app.Group("/v1")
 
 	type_user := api.Group("/typeuser")
-	type_user.Post("/", controllers.CreateType)
-	type_user.Delete("/:id", controllers.DeleteType)
+	type_user.Post("/", controllers.CreateType, middlewares.Auth)
+	type_user.Delete("/:id", controllers.DeleteType, middlewares.Auth)
 
 	status := api.Group("/status")
-	status.Post("/", controllers.CreateStatus)
-	status.Get("/", controllers.ShowStatus)
+	status.Post("/", controllers.CreateStatus, middlewares.Auth)
+	/* status.Get("/", controllers.ShowStatus)
 	status.Get("/:id", controllers.IndexStatus)
-	status.Put("/:id", controllers.UpdateStatus)
-	status.Delete("/:id", controllers.DeleteStatus)
+	status.Put("/:id", controllers.UpdateStatus) */
+	status.Delete("/:id", controllers.DeleteStatus, middlewares.Auth)
 
 	category := api.Group("/category")
 	category.Post("/", controllers.CreateType)
